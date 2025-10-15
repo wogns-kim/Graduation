46cab622b4f82eb292a08945ee28161ffdbc92f3
wogns-kim <gkgkwogns@naver.com>
2025-10-15 19:05:33 +0900

    feat: 카드 크기 및 여백 조정 완료

--- Graduation/src/pages/HomePage/index.tsx
+++ Graduation/src/pages/HomePage/index.tsx
@@ -95,7 +95,7 @@ const CardGridSection = styled.section`
 
 const CardGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
   gap: 2rem;
 `;
 
