7199c202ac679f093b63f08534a7dd0838112495
wogns-kim <gkgkwogns@naver.com>
2025-10-15 19:20:05 +0900

    최종수정

--- Graduation/src/data/cityData.ts
+++ Graduation/src/data/cityData.ts
@@ -1,9 +1,9 @@
 // src/data/cityData.ts
 export const cityData = [
-    { id: 1, name: "서울", description: "전통과 트렌드가 만나는 살아있는 도시", imageUrl: "https://images.unsplash.com/photo-1532571932369-623637146d53?w=500" },
-    { id: 2, name: "부산", description: "파도와 야경으로 에너지 가득한 도시", imageUrl: "https://images.unsplash.com/photo-1594896338556-979b99981882?w=500" },
-    { id: 3, name: "대전", description: "한국에서 만나는 과학과 자연의 도시", imageUrl: "https://images.unsplash.com/photo-1634555869321-39ab66435008?w=500" },
-    { id: 4, name: "전주", description: "한옥과 한식이 살아 숨 쉬는 도시", imageUrl: "https://images.unsplash.com/photo-1633519503482-149b1a5342a1?w=500" },
-    { id: 5, name: "여수", description: "바다와 섬이 빚어낸 낭만의 도시", imageUrl: "https://images.unsplash.com/photo-1626643449339-39906d64aa9d?w=500" },
-    { id: 6, name: "제주", description: "바다·오름·자연이 만든 천혜의 섬", imageUrl: "https://images.unsplash.com/photo-1590698933909-3c813a1e4cb?w=500" },
+    { id: 1, name: "서울", description: "전통과 트렌드가 만나는 살아있는 도시", imageUrl: "/mainpage_images/mainpage_Seoul.png"  },
+    { id: 2, name: "부산", description: "파도와 야경으로 에너지 가득한 도시", imageUrl: "/mainpage_images/mainpage_Busan.png"  },
+    { id: 3, name: "대전", description: "한국에서 만나는 과학과 자연의 도시", imageUrl: "/mainpage_images/mainpage_Daejeon.png"  },
+    { id: 4, name: "전주", description: "한옥과 한식이 살아 숨 쉬는 도시", imageUrl: "/mainpage_images/mainpage_Jeonju.png"  },
+    { id: 5, name: "여수", description: "바다와 섬이 빚어낸 낭만의 도시", imageUrl: "/mainpage_images/mainpage_Yeosu.png"  },
+    { id: 6, name: "제주", description: "바다·오름·자연이 만든 천혜의 섬", imageUrl: "/mainpage_images/mainpage_Jeju.png"  },
 ];
