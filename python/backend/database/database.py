7f60a936f79d9b30ea44b9ebd88447dda06916aa
최운형 <wh4942@gmail.com>
2025-12-16 16:31:03 +0900

    DB 연결 직전

--- python/backend/database/database.py
+++ python/backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:9047@localhost/first_trip"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/first_trip"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
